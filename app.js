0acc3aee3c01d0780c2a0546fc1da8d9f1ed58ee
Kevin Münter <Tapper12@users.noreply.github.com>
2016-02-10 11:02:38 +0200

    Prototype

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 (function(){
   "use strict";
 
+
   var Moosipurk = function(){
 
-    //SINGLETON PATTERN (4 rida)
+    // SINGLETON PATTERN (4 rida)
     if(Moosipurk.instance){
       return Moosipurk.instance;
     }
-    Moosipurk.instance = this;//This viitab moosipurgile
+    Moosipurk.instance = this; // this viitab moosipurgile
 
-    console.log("Moosipurgi sees");
+    console.log(this);
+    //console.log('moosipurgi sees');
+    //Stardin rakenduse
+    this.init();
   };
 
-  window.onload = function(){
 
+  Moosipurk.prototype = {
+
+    init: function(){
+
+      console.log("Rakendus käivitus");
+      //Siia tuleb esialgne loogika
+      //Hakka kuulama hiireklõpse
+      this.bindMouseEvents();
+    },
+    bindMouseEvents: function(){
+
+      document.querySelector('.add-new-jar').addEventListener('click',this.addNewClick.bind(this));
+    },
+    addNewClick: function(event){
+
+      console.log(event);
+    }
+  };
+
+
+  window.onload = function(){
+    var app = new Moosipurk();
   };
 
 })();
