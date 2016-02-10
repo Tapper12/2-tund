aef3cb2b148a2b6bfdace9d2058174eb916a1241
Kevin Münter <Tapper12@users.noreply.github.com>
2016-02-10 10:46:25 +0200

    Started

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+  "use strict";
+
+  var Moosipurk = function(){
+
+    console.log=('Moosipurgi sees');
+  };
+
+  window.onload = function(){
+    var app = new Moosipurk();
+
+  };
+
+})();
