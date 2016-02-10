a835c90f391e86794a4dd754f290aec60a98c082
Kevin Münter <Tapper12@users.noreply.github.com>
2016-02-10 11:21:42 +0200

    Copied clock

--- app.js
+++ app.js
@@ -16,6 +16,7 @@
 
     //Kõik muutujad, mis on üldised ja muudetavad siin
     this.click_count = 0;
+    var clock = document.getElementById('clock');
 
     this.init();
   };
@@ -28,23 +29,110 @@
       console.log("Rakendus käivitus");
       //Siia tuleb esialgne loogika
       //Hakka kuulama hiireklõpse
-      this.bindMouseEvents();
-    },
-    bindMouseEvents: function(){
+      window.setInterval(function(){
+        //Iga 500ms tagant refresh põhimõtteliselt
+        writeDate();
+      }, 500);
 
-      document.querySelector('.add-new-jar').addEventListener('click',this.addNewClick.bind(this));
-    },
-    addNewClick: function(event){
+      clock.addEventListener('click', function(event){
 
-      //console.log(event);
-      this.click_count++;
-      console.log(this.click_count);
+        if(event){
+          document.getElementById("clock").style.fontStyle = "normal";
+        }
+      });
+
+      clock.addEventListener('dblclick', function(event){
+
+        if(event){
+          document.getElementById("clock").style.fontStyle = "italic";
+        }
+      });
+
+
+    window.addEventListener('keyup', function(event){
+
+      if(event.which == 107){
+        document.getElementById("clock").style.fontSize = "800%";
+      }
+
+      if(event.which == 109){
+        document.getElementById("clock").style.fontSize = "200%";
+      }
+
+      if(event.which == 35){
+        document.getElementById("clock").style.fontSize = "500%";
+      }
+
+
+    });
+
+    window.addEventListener('keyup', function(event){
+
+      if(event.which == 38){
+        document.getElementById("clock").style.color = "green";
+        document.body.style.backgroundColor = "red";
+
+      }
+
+      if(event.which == 40){
+        document.getElementById("clock").style.color = "blue";
+        document.body.style.backgroundColor = "green";
+      }
+
+      if(event.which == 39){
+        document.getElementById("clock").style.color = "red";
+        document.body.style.backgroundColor = "blue";
+      }
+
+      if(event.which == 37){
+        document.getElementById("clock").style.color = "black";
+        document.body.style.backgroundColor = "yellow";
+      }
+
+    });
+
+
+
+    // Võtab aja ja kirjutab clock elemendi sisse
+    function writeDate(){
+
+      var today = new Date();
+
+      var day = today.getDate();
+      var year = today.getFullYear();
+      var month = today.getMonth();
+      var hours = today.getHours();
+      var minutes = today.getMinutes();
+      var seconds = today.getSeconds();
+
+      //Kuupäeva õpetuse sain siit- http://www.w3schools.com/js/js_date_methods.asp
+
+      clock.innerHTML = addZeroBefore(day) + '.' + addZeroBefore(month + 1) + '.' + year + ' ' + addZeroBefore(hours) + ':' + addZeroBefore(minutes) + ':' + addZeroBefore(seconds);
+
     }
+
+
+    function addZeroBefore(number){
+
+      if(number < 10){
+
+        number = "0" + number;
+      }
+
+      return number;
+
+
+    }
+    },
   };
 
 
   window.onload = function(){
     var app = new Moosipurk();
+    document.body.style.backgroundColor = "yellow";
+    document.getElementById("clock").style = "color: red; text-align: center; vertical-align: middle; width: 100%";
+    document.getElementById("clock").style.fontSize = "500%";
+    document.getElementById("clock").style.fontStyle = "italic";
   };
 
 })();
